e880da38331628ef4d1905ad53411964bc79181d
Heitor Saldanha <heitormontes@gmail.com>
2022-02-11 17:10:05 -0300

    fixing example project

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import { UnderflagProvider } from 'react-underflag'
-import 'react-underflag/dist/index.css'
 import Features from './components/Features'
 
 const App = () => {
-  const features = { test_a: true, test_b: false }
+  const features = { test_a: true, test_b: false, featureC: true }
 
   return (
-  <UnderflagProvider dataProvider={features} >
-    <Features />
-  </UnderflagProvider>
+    <UnderflagProvider dataProvider={features} >
+      <Features />
+    </UnderflagProvider>
   )
 }
 
--- example/src/components/Features.tsx
+++ example/src/components/Features.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import { useUnderflag } from 'react-underflag'
-import 'react-underflag/dist/index.css'
+import { useFeature, useUnderflag } from 'react-underflag'
 
 const Features = () => {
-
   const { underflag } = useUnderflag()
   const [showFeatureA, setShowFeatureA] = React.useState(false)
   const [showFeatureB, setShowFeatureB] = React.useState(false)
+  const [featureC] = useFeature(['featureC'])
 
   React.useEffect(() => {
     underflag.isOn('test_a').then(res => setShowFeatureA(res))
     underflag.isOn('test_b').then(res => setShowFeatureB(res))
-  })
+  }, [underflag])
 
   return <>
     <h1>Features</h1>
-    { showFeatureA && <h3>Feature A</h3>}
-    { showFeatureB && <h3>Feature B</h3>}
+    {showFeatureA && <h3>Feature A</h3>}
+    {showFeatureB && <h3>Feature B</h3>}
+    {featureC?.isOn() && <h3>Feature C</h3>}
   </>
 }
 
